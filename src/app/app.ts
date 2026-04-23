15cb22026005724ac30a0669b85742fa3434a0fa
amarquez10 <amarquez259@hotmail.com>
2026-04-23 15:03:22 -0500

    implement main application layout with header, sidebar, and footer components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {  EstudianteService } from './services/estudiantes';
+import { EstudianteService } from './services/estudiantes';
 import { Estudiante } from './models/estudiantes.models';
-
+import { Header } from './components/layout/header/header';
+import { Sidebar } from './components/layout/sidebar/sidebar';
+import { Footer } from './components/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule], 
+  imports: [CommonModule, Header, Sidebar, Footer], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
